afdcd8d34bd145bbe85353ff5029fa7574ae4ca2
gatti_au <gatti_au@auditeur.cnam.fr>
2025-10-18 23:00:10 +0200

    style: apply quality tools fixes

--- pyreqs/exclude.py
+++ pyreqs/exclude.py
@@ -1,12 +1,8 @@
 FOLDERS_TO_EXCLUDE = [
     ".venv",
     "__pycache__",
-    ".mypy_cache",
-    ".flake8",
-    ".gitignore",
     "LICENSE",
     "Makefile",
     "pyproject.toml",
     "README.md",
-    "TODO",
 ]
